96485e2df3afc8c7b0a6bc3e24967af3d3569d31
nznazmul <50132636+nznazmul@users.noreply.github.com>
2026-08-14 18:31:49 +0600

    fix(architecture): Export Express app directly to api/index.js with root static mirroring

--- api\[...route].js
+++
@@ -1,9 +0,0 @@
-import app from '../backend/src/server.js';
-
-export default function handler(req, res) {
-  if (req.query && req.query.route) {
-    const sub = Array.isArray(req.query.route) ? req.query.route.join('/') : req.query.route;
-    req.url = '/' + sub.replace(/^\/+/, '');
-  }
-  return app(req, res);
-}
--- api\index.js
+++ api\index.js
@@ -1,5 +1,3 @@
 import app from '../backend/src/server.js';
 
-export default function handler(req, res) {
-  return app(req, res);
-}
+export default app;
